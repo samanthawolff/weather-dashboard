5f18d87407531a9eba37e4c45ea0df9916025764
Samantha Wolff <samanthawolff07@gmail.com>
2022-03-19 17:30:15 -0500

    declare variables

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Declaring variables
+var searchBtn = $("#searchBtn");
+var formInput = $(".form-input");
+var cityInput = $(".city-input");
+var temp = $("#temp");
+var wind = $("#wind");
+var humidity = $("#humidity");
+var uvIndex = $("#uv-index");
