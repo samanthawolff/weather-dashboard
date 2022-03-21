fbc38203fcbb08b17bc0fbdaa301fe6715f18662
Samantha Wolff <samanthawolff07@gmail.com>
2022-03-20 22:04:46 -0500

    css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 // Declaring variables
 var searchBtn = $("#searchBtn");
-var formInput = $(".form-input").val().trim();
+var formInput = $(".form-input");
 var cityInput = $("#city-input");
+var currentDate = $(".current-date");
+var forecastEl = $("#curent-forecast");
 var temp = $("#temp"); 
 var wind = $("#wind");
 var humidity = $("#humidity");
 var uvIndex = $("#uv-index"); 
+
+var todaysDate = moment().format('L');
+$(".current-date").html(todaysDate);
+
  
 function getApi() {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=29.7946641&lon=-98.7319703&units=imperial&appid=9950dfaebba12c6034f0b5a4e90ddc2a';
@@ -15,34 +21,18 @@ function getApi() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            console.log(data)
 
-            for (var i = 0; i < data.length; i++) {
-                console.log("test");
-                cityInput.textContent = data[i].name;
-        };
-    })
+             for (var i = 0; i < data.length; i++) {
+                console.log("test")
+
+                $("#city-input").html(data[i].name);
+
+        }; 
+    });
 };
-
 
 $("#searchBtn").on("click", getApi);
 
-
-
-
-
-/* $("#searchBtn").on("click", function() {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=29.7946641&lon=-98.7319703&units=imperial&appid=9950dfaebba12c6034f0b5a4e90ddc2a'
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => console.log(data))
-    .catch(err => alert("wrong city name"))
-
-    .then(function(data) {
-        for (var i = 0; i < data.length; i++) {
-            cityInput.textContent = data[i].name;
-        }
-    })
-}); */
 
 
